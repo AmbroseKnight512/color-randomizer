9c4af123e623bd9f2724acb98b1edafe66aced79
ace ferreira <ambroseknight512@gmail.com>
2022-04-03 22:56:30 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#colorBtn');
+
+button.addEventListener('click', () => {
+
+    document.body.style.backgroundColor = randomizer();
+
+    function randomizer() {
+        const result = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+        return result;
+    }
+});
+
